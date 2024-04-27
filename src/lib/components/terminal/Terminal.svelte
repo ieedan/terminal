1b057e3e0ef95c9e6bbf50f09d143d3c397c031a
Aidan Bleser <aidanbleser35@gmail.com>
2024-04-27 12:21:50 -0500

    fixes for mobile view

--- src/lib/components/terminal/Terminal.svelte
+++ src/lib/components/terminal/Terminal.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <div
-	class="flex h-full w-full flex-col overflow-hidden rounded-xl
-     border border-gui-border bg-gui-background shadow-lg sm:h-3/4 sm:w-3/4"
+	class="flex h-full w-full flex-col overflow-hidden sm:rounded-xl
+     sm:border border-gui-border bg-gui-background shadow-lg sm:h-3/4 sm:w-3/4"
 >
 	<Header />
 	<Editor />
